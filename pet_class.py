9dc9e862593565a3d65fbed2fdd65bac1e712888
rsevial <rebekahsevial@gmail.com>
2023-07-01 19:14:20 +0800

    Edit setters

--- pet_class.py
+++ pet_class.py
@@ -11,14 +11,14 @@ class Pet:
         self.__age = age
 # Create setters for the three private data
     # Define method set_name
-    def set_name(self):
-        return self.__name
+    def set_name(self, name):
+        self.__name = name
     # Define method set_animal_type
-    def set_animal_type(self):
-        return self.__animal_type
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
     # Define method set_age
-    def set_age(self):
-        return self.__age
+    def set_age(self, age):
+        self.__age = age
 # Create the getters for the three private data
 # Define method get_name
 # Define method get_animal_type
