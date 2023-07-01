476fb976867cf83f7e3daa4779c7a6c8c7caed95
rsevial <rebekahsevial@gmail.com>
2023-07-01 17:10:58 +0800

    Define method for get_speed

--- car_class.py
+++ car_class.py
@@ -17,5 +17,6 @@ class Car:
     def brake(self):
         self.__speed -= 5
         return self.__speed
-
-# Define methods for get_speed
+    # Define methods for get_speed
+    def get_speed(self):
+        return self.__speed
