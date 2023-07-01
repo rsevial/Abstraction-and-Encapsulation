7481b34a8a66c11bd967e67522fa0027663dd40b
rsevial <rebekahsevial@gmail.com>
2023-07-01 19:49:02 +0800

    Create object from the answer of the user

--- pet_object.py
+++ pet_object.py
@@ -11,4 +11,5 @@ pet_animal_type = input("Enter the animal type of your pet: ")
 # Ask user the age of their pet
 pet_age = input("Enter the age of your pet: ")
 # Create object from the ans of the user
+pet_object = Pet(pet_name, pet_animal_type, pet_age)
 # Print the name, animal type and age of their pet
