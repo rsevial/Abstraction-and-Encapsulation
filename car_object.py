489ac8b9d3447d3dd94fc2735e4b00fd8cbbf321
rsevial <rebekahsevial@gmail.com>
2023-07-01 19:38:39 +0800

    Edit the font and color of the output in car_object

--- car_object.py
+++ car_object.py
@@ -3,6 +3,8 @@
 
 # Import the class from car_class
 from car_class import Car
+# import module to edit the font and color
+import fontstyle
 # Create object for Car
 car_object = Car(2020, "Inova", 0)
 # Create For loop to get the speed of the car
@@ -11,10 +13,10 @@ for i in range(5):
     car_object.accelerate()
     print("Accelerate:", car_object.get_speed())
 # Print current speed when accelarating
-print("The current speed of the car is:", car_object.get_speed(), '\n')
+print(fontstyle.apply("The current speed of the car is:", 'bold/purple/UNDERLINE'), car_object.get_speed(), '\n')
 # when braking
 for i in range(5):
     car_object.brake()
     print("Brake:", car_object.get_speed())
 # Print current speed when braking
-print("The current speed of the car is:", car_object.get_speed(), '\n')
+print(fontstyle.apply("The current speed of the car is:", 'bold/purple/UNDERLINE'), car_object.get_speed(), '\n')
