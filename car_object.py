f9ba0befa59179b2b7c438b79810b261a4e93f2e
rsevial <rebekahsevial@gmail.com>
2023-07-01 18:47:54 +0800

    Pseudocode for car_object

--- car_object.py
+++ car_object.py
@@ -1,2 +1,10 @@
 # Programmed by: Rebekah Joy E. Sevial
 # Test drive for Car class
+
+# Import the class from car_class
+# Create object for Car
+# Create For loop to get the speed of the car
+# when accelerating
+# Print current speed when accelarating
+# when braking
+# Print current speed when braking
