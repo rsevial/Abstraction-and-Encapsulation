8777a523e5707cab11968a3c3661846570f608a3
rsevial <rebekahsevial@gmail.com>
2023-07-01 19:56:46 +0800

    Print the name, animal type and age of their pet

--- pet_object.py
+++ pet_object.py
@@ -10,6 +10,9 @@ pet_name = input("Enter the name of your pet: ")
 pet_animal_type = input("Enter the animal type of your pet: ")
 # Ask user the age of their pet
 pet_age = input("Enter the age of your pet: ")
-# Create object from the ans of the user
+# Create object from the answer of the user
 pet_object = Pet(pet_name, pet_animal_type, pet_age)
 # Print the name, animal type and age of their pet
+print("\nYour pet's name is: ", pet_object.get_name())
+print("Your pet's animal type is: ", pet_object.get_animal_type())
+print("Your pet's age is: ", pet_object.get_age())
