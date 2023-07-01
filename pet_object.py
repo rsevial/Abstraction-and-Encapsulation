1166b8535c10d8263b2bc9bf03f37f1899d32587
rsevial <rebekahsevial@gmail.com>
2023-07-01 19:46:58 +0800

    Ask the user the animal type of their pet

--- pet_object.py
+++ pet_object.py
@@ -7,6 +7,7 @@ from pet_class import Pet
 # Ask user the name of their pet
 pet_name = input("Enter the name of your pet: ")
 # Ask user the animal type of their pet
+pet_animal_type = input("Enter the animal type of your pet: ")
 # Ask user the age of their pet
 # Create object from the ans of the user
 # Print the name, animal type and age of their pet
