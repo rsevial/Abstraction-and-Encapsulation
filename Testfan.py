26fe2e62d9c0452357efb15fc232676192f642e5
rsevial <rebekahsevial@gmail.com>
2023-07-01 16:36:09 +0800

    Print Output

--- Testfan.py
+++ Testfan.py
@@ -13,9 +13,13 @@ def TestFan():
     first_try.set_fan_on()
     first_try.set_radius(10)
     first_try.set_color("Yellow")
-# Print the output
+    # second try
     second_try.set_speed(2)
     second_try.set_fan_off()
     second_try.set_radius(5)
     second_try.set_color("Blue") 
+    # Print the output
+    print('First try\nSpeed: ', first_try.get_speed(), '\nRadius: ', first_try.get_radius(), '\nColor: ', first_try.get_color(), '\nOn/Off: ', first_try.get_on())
+    print('\nSecond try\nSpeed: ', second_try.get_speed(), '\nRadius: ', second_try.get_radius(), '\nColor: ', second_try.get_color(), '\nOn/Off: ', second_try.get_on())
 # Call the main function
+
