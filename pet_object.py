d47a4d87965a2ac056bbb1206aabc57d616b9de4
rsevial <rebekahsevial@gmail.com>
2023-07-01 20:03:06 +0800

    Edit the font and color of the output in pet_object

--- pet_object.py
+++ pet_object.py
@@ -3,6 +3,8 @@
 
 # Import class from pet_class
 from pet_class import Pet
+# import module to edit the font and color
+import fontstyle
 # Create object for pet
 # Ask user the name of their pet
 pet_name = input("Enter the name of your pet: ")
@@ -13,6 +15,6 @@ pet_age = input("Enter the age of your pet: ")
 # Create object from the answer of the user
 pet_object = Pet(pet_name, pet_animal_type, pet_age)
 # Print the name, animal type and age of their pet
-print("\nYour pet's name is: ", pet_object.get_name())
-print("Your pet's animal type is: ", pet_object.get_animal_type())
-print("Your pet's age is: ", pet_object.get_age())
+print("\nYour pet's name is: ", fontstyle.apply(pet_object.get_name(), 'bold/UNDERLINE/green'))
+print("Your pet's animal type is: ", fontstyle.apply(pet_object.get_animal_type(), 'bold/UNDERLINE/green'))
+print("Your pet's age is: ", fontstyle.apply(pet_object.get_age(), 'bold/UNDERLINE/green'))
