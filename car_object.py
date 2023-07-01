e5c3ca4080cdcae82eaa3c1a76831bccd270ee1a
rsevial <rebekahsevial@gmail.com>
2023-07-01 18:53:52 +0800

    Print current speed when accelarating

--- car_object.py
+++ car_object.py
@@ -8,8 +8,9 @@ car_object = Car(2020, "Inova", 0)
 # Create For loop to get the speed of the car
 # when accelerating
 for i in range(5):
-    car_object.__accelerate()
+    car_object.accelerate()
     print("Accelerate:", car_object.get_speed())
 # Print current speed when accelarating
+print("The current speed of the car is:", car_object.get_speed(), '\n')
 # when braking
 # Print current speed when braking
