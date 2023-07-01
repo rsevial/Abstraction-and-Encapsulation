4f02d15836121fcb8bea77b2a07a88e9f578872f
rsevial <rebekahsevial@gmail.com>
2023-07-01 17:05:29 +0800

    Define constructor of the Car and create a private data named year_model, make, and speed

--- car_class.py
+++ car_class.py
@@ -4,7 +4,11 @@
 # Create a class named Car
 class Car:
 # Define constructor of the Car (year_model, make, speed)
-# Create a private data for the year_model, make, speed
+    # Create a private data for the year_model, make, speed
+    def __init__(self, year_model, make, speed = 0):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = speed
 # Define methods for acceleration
 # Define methods for brake
 # Define methods for get_speed
