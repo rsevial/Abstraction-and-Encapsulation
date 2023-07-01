131cea272d2b1a6c0ae752e10625002d73067f3b
rsevial <rebekahsevial@gmail.com>
2023-07-01 18:51:16 +0800

    Create for loop to get the speed of the car when accelerating

--- car_object.py
+++ car_object.py
@@ -7,6 +7,9 @@ from car_class import Car
 car_object = Car(2020, "Inova", 0)
 # Create For loop to get the speed of the car
 # when accelerating
+for i in range(5):
+    car_object.__accelerate()
+    print("Accelerate:", car_object.get_speed())
 # Print current speed when accelarating
 # when braking
 # Print current speed when braking
